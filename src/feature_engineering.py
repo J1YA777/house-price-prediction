b8d114fe7b45a3fd9027a5d301ba847486986e77
Jiya <jiyaeshaal1@gmail.com>
2025-12-05 14:23:19 +0500

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+def add_interaction_features(df: pd.DataFrame) -> pd.DataFrame:
+    """Add advanced interaction features."""
+    df = df.copy()
+    df["OverallQual_x_TotalSF"] = df["OverallQual"] * (df["TotalBsmtSF"].fillna(0) + df["1stFlrSF"].fillna(0) + df["2ndFlrSF"].fillna(0))
+    df["TotalRooms"] = df[["BedroomAbvGr", "FullBath", "HalfBath", "KitchenAbvGr"]].sum(axis=1)
+    return df
 
